8e799c2a35249523199879cdbf368fbb3816acee
qianxug <qianxug@gmail.com>
2023-08-19 09:44:23 -0400

    made navbar, more homepage design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import WebFont from 'webfontloader';
+
 import './App.css';
 import Home from './pages/Home';
 import Category from './pages/Category';
+import TopNavBar from './components/TopNavBar';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const allCategories = ["career", "academics", "interpersonal relationships", "personal development"]
   
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['EB Garamond']
+      }
+    });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-        { allCategories.map((category) => <Route path={`/${category.replaceAll(" ", "-")}`} element={<Category categories={allCategories} category={category} />} key={category} />) }
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <TopNavBar categories={allCategories} />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          { allCategories.map((category) => <Route path={`/${category.replaceAll(" ", "-")}`} element={<Category categories={allCategories} category={category} />} key={category} />) }
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
